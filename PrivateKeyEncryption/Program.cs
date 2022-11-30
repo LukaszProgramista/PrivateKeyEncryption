9de465d0e720c3a81d4a824dce7a2a006eb2d77f
Łukasz Głowacki <lukaszglowacki112@gmail.com>
2022-11-30 19:04:15 +0100

    Add project files.

---
+++ PrivateKeyEncryption/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateKeyEncryption
+{
+   
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            RsaEncryption rsa = new RsaEncryption();
+            string cypher = string.Empty;
+            Console.WriteLine($"Klucz publiczny {rsa.GetPublicKey()}");
+
+            Console.WriteLine("Proszę podaj twój text do zaszyfrowania:");
+            var text = Console.ReadLine();
+            if (!string.IsNullOrEmpty(text))
+            {
+                cypher = rsa.Encrypt(text);
+                Console.WriteLine($"Encrypted Text: {cypher}");
+            }
+
+            Console.WriteLine("Nacisnij klawisz aby odszyfrować twój tekst");
+            Console.ReadLine();
+            var plainText = rsa.Decrypt(cypher);
+
+            Console.WriteLine($"Decrypted Message: {plainText}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrivateKeyEncryption/RsaEncryption.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PrivateKeyEncryption
+{
+    public class RsaEncryption
+    {
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RsaEncryption()
+        {
+            _privateKey = csp.ExportParameters(true);
+            _publicKey = csp.ExportParameters(false);
+        }
+
+        public string GetPublicKey()
+        {
+            var sw = new StringWriter();
+            var XS = new XmlSerializer(typeof(RSAParameters));
+            XS.Serialize(sw, _publicKey);
+            return sw.ToString();
+        }
+        public string Encrypt(string plainText)
+        {
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(_publicKey);
+            var data = Encoding.Unicode.GetBytes(plainText);
+            var cypher = csp.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
+        }
+        public string Decrypt(string cypherText)
+        {
+            var dataBytes = Convert.FromBase64String(cypherText);
+            csp.ImportParameters(_privateKey);
+            var plainText = csp.Decrypt(dataBytes, false);
+            return Encoding.Unicode.GetString(plainText);
+        }
+    }
+}
